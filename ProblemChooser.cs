6928a69653a9b29889c5c37321ddc86c0053dec5
Ryan Ball <ballrj@vcu.edu>
2014-06-28 03:19:09 -0400

    Problem Chooser
    The basics of the ProblemChooser are working now, but there's nothing to
    choose yet.

--- ProblemChooser.cs
+++ ProblemChooser.cs
@@ -1,5 +1,6 @@
 //Ryan Ball
 using System;
+using System.Collections.Generic;
 
 namespace EulerProblems
 {
@@ -7,8 +8,38 @@ namespace EulerProblems
     {
 	static void Main(string[] args)
 	{
-	    IEulerProblem p2 = new Problem2();
-	    Console.WriteLine(p2.GetAnswer());
+	    Dictionary<int, IEulerProblem> problems = new Dictionary<int, IEulerProblem>();
+	    problems.Add(2, new Problem2());
+	    string userInput = "";
+	    int inputInt;
+	    IEulerProblem currentProblem;
+	    while(userInput.ToLower() != "q")
+	    {
+		Console.WriteLine("--------------------------");
+		Console.WriteLine("Available Euler Project problems are:");
+		Console.WriteLine("2. Even Fibonacci numbers\n");
+		Console.WriteLine("Type a number to see the answer. Type 'q' to quit.\n");
+		userInput = Console.ReadLine();
+		try
+		{
+		    if(userInput.ToLower() != "q")
+		    {
+			inputInt = Convert.ToInt32(userInput);
+			currentProblem = problems[Convert.ToInt32(userInput)];
+			Console.WriteLine("\nThe answer is:");
+			Console.WriteLine(currentProblem.GetAnswer());
+			Console.WriteLine();
+		    }
+		}
+		catch(FormatException)
+		{
+		    Console.WriteLine("Please type a number.");
+		}
+		catch(KeyNotFoundException)
+		{
+		    Console.WriteLine("That solution is not available.");
+		}
+	    }
 	}
     }
 }
